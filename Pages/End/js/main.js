28668bc3facd3081bd1f46547d5820b3860b1251
Davino Rosaria <doubled.rosaria@gmail.com>
2020-11-04 20:53:03 +0100

    End page added

---
+++ Pages/End/js/main.js
@@ -0,0 +1,11 @@
+var input = 0;
+var textField_0 = '5 miljoen doden sinds 1988';
+var textField_1 = 'Waarvan 40% kind';
+var textField_2 = '200,000+ vrouwen en kinderen verkracht';
+var textField_3 = '20,000+ gorillas en andere bedreigde diersoorten gedood';
+var textField_4 = 'Mensen worden niet voldoende uitbetaald voor de werk die ze in de mijnen verrichten';
+var textField_5 = 'Kinderarbeid';
+
+function typing(){
+    if(input < textField)
+}
