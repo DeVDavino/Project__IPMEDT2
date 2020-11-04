c002f1132b9572dc55724b21e856263fc51920f8
Abel Adema <abeladema@hotmail.com>
2020-11-04 12:11:07 +0100

    merchant

--- Pages/Merchant/js/main.js
+++ Pages/Merchant/js/main.js
@@ -1,18 +1,18 @@
 var i = 0;
-var txt = 'Verzameld: 0.4 kg';
+var txt = 'Verzameld:      0.4 kg';
 var txt1 = 'prijs: $10 per kg ';
 var txt2 = 'prijs: $5 per kg ';
 var txt3 = 'prijs: $5 per kg ';
-var txt4 = '$ 4';
-var txt5 = '$ 2 ';
-var txt6 = '$ 2 ';
-var txt7 = '$ 8';
+var txt4 = '$                           4';
+var txt5 = '$                                2';
+var txt6 = '$                                     2';
+var txt7 = '                                           8';
 var speed = 100;
 var speed1 = 500;
 var calcTracker = 0;
 
 function typeWriter() {
-  if (i < txt1.length) {
+  if (i < txt7.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     document.getElementById("demo1").innerHTML += txt.charAt(i);
     document.getElementById("demo2").innerHTML += txt.charAt(i);
