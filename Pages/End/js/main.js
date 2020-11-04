010e554e56754b9612d7b68f3ebe78b409a82dbd
Abel Adema <abeladema@hotmail.com>
2020-11-04 23:15:11 +0100

    end functie

--- Pages/End/js/main.js
+++ Pages/End/js/main.js
@@ -1,15 +1,21 @@
-var input = 0;
-var textFieldTime = '                         ';
+var i = 0;
 var textField_0 = '5 miljoen doden sinds 1988';
 var textField_1 = 'Waarvan 40% kind';
 var textField_2 = '200,000+ vrouwen en kinderen verkracht';
 var textField_3 = '20,000+ gorillas en andere bedreigde diersoorten gedood';
-var textField_4 = 'Mensen worden niet voldoende uitbetaald voor de werk die ze in de mijnen verrichten';
+var textField_4 = 'Mensen worden niet voldoende uitbetaald voor het werk die ze in de mijnen verrichten';
 var textField_5 = 'Kinderarbeid';
 var speed = 100;
 
+window.onscroll = function(){
+    if(window.scrollY >= 120 && window.scrollY <= 140){
+        typeWriter();
+    }
+}
+
+
 function typeWriter() {
-    if (input < textFieldTime.length) {
+    if (i < textField_4.length) {
         document.getElementById("demo0__text--js").innerHTML += textField_0.charAt(i);
         document.getElementById("demo1__text--js").innerHTML += textField_1.charAt(i);
         document.getElementById("demo2__text--js").innerHTML += textField_2.charAt(i);
