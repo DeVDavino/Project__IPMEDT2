c5d216e35da1cc9e2831fd7bdca6cb78c688c1a4
Davino Rosaria <doubled.rosaria@gmail.com>
2020-11-04 21:51:21 +0100

    beautify

--- Pages/End/js/main.js
+++ Pages/End/js/main.js
@@ -8,15 +8,15 @@ var textField_4 = 'Mensen worden niet voldoende uitbetaald voor de werk die ze i
 var textField_5 = 'Kinderarbeid';
 var speed = 100;
 
-function typeWriter(){
-    if(input < textFieldTime.length){
+function typeWriter() {
+    if (input < textFieldTime.length) {
         document.getElementById("demo0__text--js").innerHTML += textField_0.charAt(i);
         document.getElementById("demo1__text--js").innerHTML += textField_1.charAt(i);
         document.getElementById("demo2__text--js").innerHTML += textField_2.charAt(i);
         document.getElementById("demo3__text--js").innerHTML += textField_3.charAt(i);
         document.getElementById("demo4__text--js").innerHTML += textField_4.charAt(i);
         document.getElementById("demo5__text--js").innerHTML += textField_5.charAt(i);
-        
+
         i++;
         setTimeout(typeWriter, speed);
     }
