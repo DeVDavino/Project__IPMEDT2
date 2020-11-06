e5d25152a579d522b2232e55c1c6a8011a36eb32
Abel Adema <abeladema@hotmail.com>
2020-11-06 11:32:14 +0100

    scheiden js

--- Pages/Scheiden/js/main.js
+++ Pages/Scheiden/js/main.js
@@ -37,74 +37,26 @@ function check() {
 }
 
 
-function onClickEvent1() {
-    firstMineral.classList.add("container__item1--active");
+function onClickEvent(thisElement) {
     audio.play();
     console.log(clicks);
     clicks++;
+    if(thisElement.id == "container__item--1"){
+        thisElement.classList.add("container__item1--active");
+    }else if(thisElement.id == "container__item--2"){
+        thisElement.classList.add("container__item2--active");
+    }else if(thisElement.id == "container__item--3"){
+        thisElement.classList.add("container__item3--active");
+    }else if(thisElement.id == "container__item--4"){
+        thisElement.classList.add("container__item4--active");
+    }else if(thisElement.id == "container__item--5"){
+        thisElement.classList.add("container__item5--active");
+    }else if(thisElement.id == "container__item--6"){
+        thisElement.classList.add("container__item6--active");
+    }
     document.getElementById("mineral__counter--js").innerHTML = clicks;
     document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    firstMineral.removeAttribute("onclick");
-    firstMineral.style.opacity = "0";
-    return clicks;
-}
-
-function onClickEvent2() {
-    secondMineral.classList.add("container__item2--active");
-    audio.play();
-    console.log(clicks);
-    clicks++;
-    document.getElementById("mineral__counter--js").innerHTML = clicks;
-    document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    secondMineral.removeAttribute("onclick");
-    secondMineral.style.opacity = "0";
-    return clicks;
-}
-
-function onClickEvent3() {
-    thirdMineral.classList.add("container__item3--active");
-    audio.play();
-    console.log(clicks);
-    clicks++;
-    document.getElementById("mineral__counter--js").innerHTML = clicks;
-    document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    thirdMineral.removeAttribute("onclick");
-    thirdMineral.style.opacity = "0";
-    return clicks;
-}
-
-function onClickEvent4() {
-    forthMineral.classList.add("container__item4--active");
-    audio.play();
-    console.log(clicks);
-    clicks++;
-    document.getElementById("mineral__counter--js").innerHTML = clicks;
-    document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    forthMineral.removeAttribute("onclick");
-    forthMineral.style.opacity = "0";
-    return clicks;
-}
-
-function onClickEvent5() {
-    fifthMineral.classList.add("container__item5--active");
-    audio.play();
-    console.log(clicks);
-    clicks++;
-    document.getElementById("mineral__counter--js").innerHTML = clicks;
-    document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    fifthMineral.removeAttribute("onclick");
-    fifthMineral.style.opacity = "0";
-    return clicks;
-}
-
-function onClickEvent6() {
-    sixthMineral.classList.add("container__item6--active");
-    audio.play();
-    console.log(clicks);
-    clicks++;
-    document.getElementById("mineral__counter--js").innerHTML = clicks;
-    document.getElementById("mineral__counter2--js").innerHTML = clicks / 5 + " kg";
-    sixthMineral.removeAttribute("onclick");
-    sixthMineral.style.opacity = "0";
+    thisElement.removeAttribute("onclick");
+    thisElement.style.opacity = "0";
     return clicks;
 }
