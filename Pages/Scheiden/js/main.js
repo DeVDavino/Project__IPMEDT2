c9f705005b57fb80bebdbf5185191a21f95b1f65
Abel Adema <abeladema@hotmail.com>
2020-12-03 12:15:59 +0100

    srcset minerals toegevoegd

--- Pages/Scheiden/js/main.js
+++ Pages/Scheiden/js/main.js
@@ -1,9 +1,9 @@
-var firstMineral = document.getElementById('container__item--1');
-var secondMineral = document.getElementById('container__item--2');
-var thirdMineral = document.getElementById('container__item--3');
-var forthMineral = document.getElementById('container__item--4');
-var fifthMineral = document.getElementById('container__item--5');
-var sixthMineral = document.getElementById('container__item--6');
+var firstMineral = document.getElementById('js--container__item--1');
+var secondMineral = document.getElementById('js--container__item--2');
+var thirdMineral = document.getElementById('js--container__item--3');
+var forthMineral = document.getElementById('js--container__item--4');
+var fifthMineral = document.getElementById('js--container__item--5');
+var sixthMineral = document.getElementById('js--container__item--6');
 let itemsContainer = document.getElementById('dropbox');
 var paragraph = document.getElementById('footer__counter--js');
 var button = document.getElementById('js--button');
@@ -40,21 +40,21 @@ function check() {
 function onClickEvent(thisElement) {
     audio.play();
     clicks++;
-    if(thisElement.id == "container__item--1"){
+    if(thisElement.id == "js--container__item--1"){
         thisElement.classList.add("container__item1--active");
-    }else if(thisElement.id == "container__item--2"){
+    }else if(thisElement.id == "js--container__item--2"){
         thisElement.classList.add("container__item2--active");
-    }else if(thisElement.id == "container__item--3"){
+    }else if(thisElement.id == "js--container__item--3"){
         thisElement.classList.add("container__item3--active");
-    }else if(thisElement.id == "container__item--4"){
+    }else if(thisElement.id == "js--container__item--4"){
         thisElement.classList.add("container__item4--active");
-    }else if(thisElement.id == "container__item--5"){
+    }else if(thisElement.id == "js--container__item--5"){
         thisElement.classList.add("container__item5--active");
-    }else if(thisElement.id == "container__item--6"){
+    }else if(thisElement.id == "js--container__item--6"){
         thisElement.classList.add("container__item6--active");
     }
-    document.getElementById("footer__counter--js").innerHTML = clicks;
-    document.getElementById("footer__counter2--js").innerHTML = clicks / 5 + " kg";
+    document.getElementById("js--footer__counter").innerHTML = clicks;
+    document.getElementById("js--footer__counter2").innerHTML = clicks / 5 + " kg";
     thisElement.removeAttribute("onclick");
     thisElement.style.opacity = "0";
     return clicks;
